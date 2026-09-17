184cea3f1f30ad2bc961f02fdced5273691978c4
Yago Silva <yglimabem10@gmail.com>
2026-09-17 10:58:23 -0300

    Primeiro commit

---
+++ aula01.py
@@ -0,0 +1,11 @@
+#O super() serve para chamar a classe pai dentro de uma classe filha 
+
+class MyString(str):
+    def upper (self):
+        print("call upper")
+        call = super().upper()
+        print("after upper")
+        return call
+
+string= MyString('Yago')
+print(string.upper())
